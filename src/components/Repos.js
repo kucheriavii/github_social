49369c1bb2b9f5aa91c83742c129bba58c42c2c9
kucheriavii <kucheriavii@gmail.com>
2023-09-19 22:00:04 +0300

    287 - Calculate Most Used Language

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -4,7 +4,23 @@ import { GithubContext } from '../context/context';
 import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from './Charts';
 const Repos = () => {
   const {repos} = useContext(GithubContext);
-  
+  let languages = repos.reduce((total, item) => {
+    const {language} = item;
+    if (!language){
+      return total
+    }
+    if(!total[language]){
+      total[language] = {label:language, value:1}
+    } else {
+      total[language] = {...total[language], value:total[language].value+1 }
+    }
+    return total;    
+  }, {})
+
+  languages = Object.values(languages).sort((a,b)=>{
+    return b.value - a.value
+  }).slice(0,5);
+
   const data = [
     {
       label: "HTML",
@@ -22,7 +38,7 @@ const Repos = () => {
   return <section className='section'>
     <Wrapper className='section-center'>
       {/* <ExampleChart data={data}/> */}
-      <Pie3D data={data}/>
+      <Pie3D data={languages}/>
     </Wrapper>
   </section>;
 };
