1650d1c0481d8f1d4852b7d8eb76601d0509ac97
kucheriavii <kucheriavii@gmail.com>
2023-09-24 15:12:25 +0300

    301 - Auth0 Setup

--- src/index.js
+++ src/index.js
@@ -7,10 +7,17 @@ import { GithubProvider } from './context/context';
 import { Auth0Provider } from '@auth0/auth0-react';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  //dev-hcc4ojgr73drepam.us.auth0.com
+  //5xmTLdqAbuvtJmvlHSCv9r4pV2y6eCLb
   <React.StrictMode>
-    <GithubProvider>
-      <App />
-    </GithubProvider>
+    <Auth0Provider
+        domain='dev-hcc4ojgr73drepam.us.auth0.com'
+        clientId='5xmTLdqAbuvtJmvlHSCv9r4pV2y6eCLb'
+        redirectUri={window.location.origin}>
+      <GithubProvider>
+        <App />
+      </GithubProvider>
+    </Auth0Provider>
   </React.StrictMode>
 );
 
